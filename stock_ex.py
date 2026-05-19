d8358ae1a7334286c2157c47eccf37d97758ea21
klamath608 <klamath608@gmail.com>
2026-05-19 17:58:07 +0800

    Update stock_ex.py

--- stock_ex.py
+++ stock_ex.py
@@ -192,7 +192,7 @@ import os
 os.makedirs("reports", exist_ok=True)
 
 #假設 df_sorted 是你排序後的 DataFrame
-excel_path = "reports/report.xlsx"
+excel_path = "docs/report.xlsx"
 
 # 寫入 Excel，不含索引欄
 df_sorted.to_excel(excel_path, index=False)
